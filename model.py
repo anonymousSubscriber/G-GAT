0056973b882b1c27ed83bff7c281a7f5342a5a9e
anonymous <anonymousSubscriber32790@proton.me>
2022-10-09 16:12:55 +0800

    finish GNN model

--- model.py
+++ model.py
@@ -1,38 +1,25 @@
-from turtle import forward
 import torch.nn as nn
 import torch
 import math
 import numpy as np
 import torch.nn.functional as F
 import torch.optim as optim
-
-class GCN1(nn.Module):
-    def __init__(self,input_dim,output_dim,adj,act='None'):
-        super(GCN1,self).__init__()
-        self.fc=nn.Linear(input_dim,output_dim)
-        self.adj=adj
-        self.act=act
-    def forward(self,x):
-        x=self.fc(x)
-        x=torch.mm(self.adj,x)
-        if self.act !='None':
-            x=F.relu(x)
-        return x
+from GNN_model import *
 def build_QK(input_dim,output_dim,build_att,att_act,adj):
     if build_att=='GCN1':
        return GCN1(input_dim,output_dim,adj,att_act)
     elif build_att=='GCN2':
-        pass
+        return GCN2(input_dim,output_dim,adj,att_act)
     elif build_att=='GAT1':
-        pass
+        return GAT1(input_dim,output_dim,adj,att_act)
     elif build_att=='GAT2':
-        pass
+        return GAT2(input_dim,output_dim,adj,att_act)
     elif build_att=='GBP':
-        pass
+        return GBP(input_dim,output_dim,adj,att_act)
     elif build_att=="SGC":
-        pass
+        return SGC(input_dim,output_dim,adj,att_act)
     elif build_att=='linear':
-        pass
+        return linear_transformation(input_dim,output_dim,adj,att_act)
     else:
         raise
 
@@ -86,63 +73,3 @@ class GGAT(nn.Module):
 
         return F.log_softmax(out, dim=1)
 
-
-
-# def build_model(args,num_features,num_labels,device,adj):
-#     if args.model_method=='gcn':
-#         model = GCN(nfeat=num_features,
-#                 nhid=args.nhid,
-#                 nclass=num_labels,adj=adj).to(device)
-
-
-#         optimizer_sett = [
-#         {'params': model.classifier.parameters(), 'weight_decay': args.w_fc2, 'lr': args.lr_fc},
-#         {'params': model.fc.parameters(), 'weight_decay': args.w_fc1, 'lr': args.lr_fc},
-#         ]
-#         optimizer = optim.Adam(optimizer_sett)
-#         return model,optimizer
-#     if args.model_method=='gat3':
-#         adj_list = []
-#         adj_list.append(None)
-#         no_loop_mat = torch.eye(adj.shape[0]).to(device)
-
-#         for ii in range(args.nlayers):
-#             no_loop_mat = torch.mm(adj, no_loop_mat)
-#             adj_list.append(no_loop_mat)
-
-#         model = FSGNN(nfeat=num_features,
-#                 nhid=args.nhid,
-#                 nclass=num_labels,
-#                 dropout=args.dropout,
-#                 adj_list=adj_list,
-#                 adj_att=adj).to(device)
-
-
-#         optimizer_sett = [
-#         {'params': model.fc2.parameters(), 'weight_decay': args.w_fc2, 'lr': args.lr_fc},
-#         {'params': model.fc1.parameters(), 'weight_decay': args.w_fc1, 'lr': args.lr_fc},
-#         {'params': model.att, 'weight_decay': args.w_att, 'lr': args.lr_att},
-#         {'params': model.Q.parameters(), 'weight_decay': args.w_fc1/4, 'lr': args.lr_fc},
-#         {'params': model.K.parameters(), 'weight_decay': args.w_fc1/4, 'lr': args.lr_fc},
-#         {'params': model.w, 'weight_decay': 0.0, 'lr': args.lr_fc},
-#         ]
-
-#         optimizer = optim.Adam(optimizer_sett)
-#         return model,optimizer
-#     if args.model_method=='gat':
-#         model = GAT(nfeat=num_features,
-#                 nhid=args.nhid,
-#                 nclass=num_labels,adj=adj).to(device)
-
-
-#         optimizer_sett = [
-#         {'params': model.classifier.parameters(), 'weight_decay': args.w_fc2, 'lr': args.lr_fc},
-#         {'params': model.fc.parameters(), 'weight_decay': args.w_fc1, 'lr': args.lr_fc},
-#         {'params': model.a1.parameters(), 'weight_decay': args.w_fc1, 'lr': args.lr_fc},
-#         {'params': model.a2.parameters(), 'weight_decay': args.w_fc1, 'lr': args.lr_fc}
-#         ]
-#         optimizer = optim.Adam(optimizer_sett)
-#         return model,optimizer
-#     else:
-#         print("can't find that model")
-#         raise
